5d027128febaf1a0b283b6b8e39b7ca7305b05fc
Flemming Behrend <topfeb@gmail.com>
2023-04-24 11:30:42 +0200

    added auto publish of github pages

---
+++ src/components/test-component/test-component.stories.tsx
@@ -0,0 +1,14 @@
+import * as React from 'react';
+import TestComponent from './test-component';
+
+export default {
+  component: TestComponent,
+  title: 'TestComponent'
+};
+
+const Template = (args) => <TestComponent {...args} />;
+
+export const Default = Template.bind({});
+Default.args = {
+  label: 'Default'
+};
